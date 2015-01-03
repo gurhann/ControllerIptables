1eaa40c23b794042ac129b5873737406004f0356
gkucuk <gkucuk@firat.edu.tr>
2015-01-03 20:36:45 +0200

    solve the managedbean send command to server problem

--- src/main/java/com/dursunc/bilgiguvenlik/connectSsh.java
+++ src/main/java/com/dursunc/bilgiguvenlik/connectSsh.java
@@ -6,11 +6,14 @@
 package com.dursunc.bilgiguvenlik;
 
 import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -37,33 +40,63 @@ public class connectSsh implements Serializable {
 
     public void loginSsh() throws JSchException {
         try {
-            int port = 22;
-            JSch jsch = new JSch();
-            jschSession = jsch.getSession(user.getUserName(), user.getIp(), port);
-            jschSession.setPassword(user.getPassWord());
+             
             java.util.Properties config = new java.util.Properties();
             config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+             jschSession=jsch.getSession(user.getUserName(), user.getIp(), 22);
+            jschSession.setPassword(user.getPassWord());
+            jschSession.setUserInfo(new SshUserInfo(user.getPassWord()));
             jschSession.setConfig(config);
             jschSession.connect();
-            channel = jschSession.openChannel("sftp");
+            System.out.println("Connected");
+             
+             channel=jschSession.openChannel("exec");
+            ((ChannelExec)channel).setCommand("sudo chmod 777 -R /home/gurhan/fal");
+            
+            ((ChannelExec)channel).setPty(true);
+            channel.setInputStream(null);
+            ((ChannelExec)channel).setErrStream(System.err);
+             
+            InputStream in=channel.getInputStream();
+            OutputStream out = channel.getOutputStream();
             channel.connect();
-            DataInputStream dataIn = new DataInputStream(channel.getInputStream());
-            DataOutputStream dataOut = new DataOutputStream(channel.getOutputStream());
-
-            // send ls command to the server  
-            dataOut.writeBytes("ls -la\r\n");
-            dataOut.flush();
-
-            // and print the response   
-            String line = dataIn.readLine();
-            System.out.println(line);
-            while (!line.endsWith(endLineStr)) {
-                System.out.println(line);
-                line = dataIn.readLine();
+            out.write((user.getPassWord()+"\n").getBytes());
+            out.flush();
+   
+            
+            System.out.println(in.available());
+            byte[] tmp = new byte[1024];
+            int count = 0;
+            while (true) {
+                System.out.println(in.available());
+                while (in.available() > 0) {
+                    count = 0;
+                    int i = in.read(tmp, 0, 1024);
+                    if (i < 0) {
+                        break;
+                    }
+                    System.out.print(new String(tmp, 0, i));
+                }if(in.available() == 0) {
+                    count++;
+                }if(count == 5) {
+                    channel.disconnect();
+                    break;
+                }
+                if (channel.isClosed()) {
+                    System.out.println("exit-status: " + channel.getExitStatus());
+                    break;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ee) {
+                }
             }
-
+            channel.disconnect();
+            jschSession.disconnect();
+            System.out.println("DONE");
         } catch (Exception e) {
-            System.out.println("+++++++++++++" + e);
+            System.out.println("+++++++++++++" + e.toString());
             FacesContext context = FacesContext.getCurrentInstance();
             context.addMessage(null, new FacesMessage("Bağlantı Sağlanamadı", "Lütfen bilgilerinizi kontrol ediniz!"));
         }
